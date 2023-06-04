22b518dd55856d426c04f3d4a8a6cf05a11a3fa2
RightoRen <69438543+RightoRen@users.noreply.github.com>
2023-06-04 16:08:09 -0400

    Add files via upload
    Code Finalized

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 console.log("Javascript is connected");
+
+const click=document.querySelector;
+
+Small.addEventListener('click', smallMessage);
+
+Medium.addEventListener('click', mediumMessage);
+
+Tall.addEventListener('click', tallMessage);
+
+Bub.addEventListener('click', bubMessage);
+
+BigBottle.addEventListener('click', bigMessage);
+
+Square.addEventListener('click', squareMessage);
+
+function smallMessage() {
+    console.log("Small Clicked");
+};
+
+function mediumMessage() {
+    console.log("Medium Clicked");
+};
+
+function tallMessage() {
+    console.log("Tall Clicked");
+};
+
+function bubMessage() {
+    console.log("Bub Clicked");
+};
+
+function bigMessage() {
+    console.log("Big Clicked");
+};
+
+function squareMessage() {
+    console.log("Square Clicked");
+};
